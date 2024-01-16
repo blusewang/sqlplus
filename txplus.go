157932817f267ebb660d76de70363f7eae16a44f
jeff <jeff@wangjunfeng.com.cn>
2024-01-17 00:41:35 +0800

    支持ctx

--- txplus.go
+++ txplus.go
@@ -1,6 +1,9 @@
 package sqlplus
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type TxPlus struct {
 	*sql.Tx
@@ -37,6 +40,50 @@ func (tx TxPlus) QuerySlice(list interface{}, query string, args ...interface{})
 	}
 
 	b.rows, err = tx.Query(query, args...)
+	if err != nil {
+		return
+	}
+	defer b.rows.Close()
+
+	err = b.parseSlideAll()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (tx TxPlus) QueryStructContext(ctx context.Context, obj interface{}, query string, args ...interface{}) (err error) {
+	var b binder
+
+	err = b.analysisStruct(obj)
+	if err != nil {
+		return
+	}
+
+	b.rows, err = tx.QueryContext(ctx, b.mustLimit1(query), args...)
+	if err != nil {
+		return
+	}
+	defer b.rows.Close()
+
+	err = b.parseStruct()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (tx TxPlus) QuerySliceContext(ctx context.Context, list interface{}, query string, args ...interface{}) (err error) {
+	var b binder
+
+	err = b.analysisSlice(list)
+	if err != nil {
+		return
+	}
+
+	b.rows, err = tx.QueryContext(ctx, query, args...)
 	if err != nil {
 		return
 	}
